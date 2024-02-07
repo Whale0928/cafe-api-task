c6ef48fcf57f29d0de776f53a8ce9e94112675c0
KIM HYUN GI <rlagusrl928@gmail.com>
2024-02-07 17:59:03 +0900

    chore: create the project

---
+++ src/main/java/app/practice/cafeapitask/CafeApiTaskApplication.java
@@ -0,0 +1,13 @@
+package app.practice.cafeapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeApiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeApiTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/practice/cafeapitask/CafeApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package app.practice.cafeapitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApiTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
