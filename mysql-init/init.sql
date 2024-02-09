88628af8925e051629bf141ecc6b69d96aee04c4
KIM HYUN GI <rlagusrl928@gmail.com>
2024-02-09 17:36:11 +0900

    chore: fixed docker init

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -3,12 +3,45 @@ CREATE TABLE IF NOT EXISTS owner
     id           BIGINT AUTO_INCREMENT PRIMARY KEY,
     phone_number VARCHAR(255) NOT NULL UNIQUE,
     password     VARCHAR(255) NOT NULL
-);
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+;
 
+CREATE TABLE IF NOT EXISTS product
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    owner_id        BIGINT       NOT NULL,
+    category        VARCHAR(255) NOT NULL,
+    price           DECIMAL      NOT NULL,
+    cost            DECIMAL      NOT NULL,
+    name            VARCHAR(255) NOT NULL,
+    description     TEXT         NOT NULL,
+    barcode         VARCHAR(255) NOT NULL,
+    expiration_date DATE         NOT NULL,
+    size            VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_product_owner FOREIGN KEY (owner_id) REFERENCES owner (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+;
 
 INSERT INTO owner (phone_number, password)
 VALUES ('02012341234', '$2a$10$Hl0fukY71RY3AXiNN6OH8OkX5HFQwW6QQz3Bkc0m6iXHH5UoChK7m'),
        ('01012341234', '$2a$10$Hl0fukY71RY3AXiNN6OH8OkX5HFQwW6QQz3Bkc0m6iXHH5UoChK7m'),
        ('029415112', '$2a$10$Hl0fukY71RY3AXiNN6OH8OkX5HFQwW6QQz3Bkc0m6iXHH5UoChK7m'),
        ('01031235592', '$2a$10$Hl0fukY71RY3AXiNN6OH8OkX5HFQwW6QQz3Bkc0m6iXHH5UoChK7m')
-ON DUPLICATE KEY UPDATE phone_number= VALUES (phone_number), password =VALUES(password);
+ON DUPLICATE KEY UPDATE phone_number= VALUES(phone_number),
+                        password    =VALUES(password);
+
+
+INSERT INTO product (owner_id, category, price, cost, name, description, barcode, expiration_date, size)
+VALUES (1, '음료', 3.50, 1.50, 'Latte', '크리미하고 풍부한 라떼', '1234567890123', '2023-12-31', 'LARGE'),
+       (1, '패스트리', 2.00, 0.80, 'Croissant', '버터가 풍부하고 겹겹이 층이 있는 크루아상', '2345678901234', '2023-06-30', 'SMALL'),
+       (1, '스낵', 1.50, 0.50, 'Bagel', '부드럽고 쫄깃한 베이글', '3456789012345', '2023-11-30', 'LARGE'),
+       (2, '음료', 4.00, 2.00, 'Cappuccino', '부드럽고 진한 카푸치노', '3456789012345', '2024-01-31', 'SMALL'),
+       (2, '스낵', 1.50, 0.50, 'Bagel', '부드럽고 쫄깃한 베이글', '4567890123456', '2023-11-30', 'LARGE'),
+       (3, '음료', 3.00, 1.00, 'Americano', '깔끔하고 진한 아메리카노', '5678901234567', '2023-12-31', 'MEDIUM'),
+       (3, '패스트리', 2.50, 1.00, 'Muffin', '부드럽고 달콤한 머핀', '6789012345678', '2023-06-30', 'MEDIUM'),
+       (4, '음료', 3.50, 1.50, 'Latte', '크리미하고 풍부한 라떼', '7890123456789', '2023-12-31', 'LARGE'),
+       (4, '패스트리', 2.00, 0.80, 'Croissant', '버터가 풍부하고 겹겹이 층이 있는 크루아상', '8901234567890', '2023-06-30', 'SMALL')
